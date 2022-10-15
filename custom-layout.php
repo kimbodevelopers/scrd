e735f5a6cb86a6004781027bcf6a5234e202f0e5
timng93 <tim@kimbodesign.ca>
2022-10-14 17:13:38 -0700

    add custom layout

--- custom-layout.php
+++ custom-layout.php
@@ -1,8 +1,7 @@
 <?php defined('ABSPATH') or die(""); ?>
 <?php get_header(); 
-/* Template Name: Water */
+/* Template Name: Custom Layout */
 ?>
-
 
 <div class="container-fluid site-component-container t1-container">
     <div class="row site-component-row t1-row">
@@ -14,15 +13,16 @@
     </div>
 </div>
 
-<?php if( have_rows('water_content') ): ?>
-    <?php while( have_rows('water_content') ): the_row(); ?>
+<?php if( have_rows('custom_content') ): ?>
+    <?php while( have_rows('custom_content') ): the_row(); ?>
         <?php get_template_part('inc/components/t1-text-group') ?>
         <?php get_template_part('inc/components/b1-table') ?>
+        <?php get_template_part('inc/components/p2-picture-text') ?>
         <?php get_template_part('inc/components/p3-map') ?>
         <?php get_template_part('inc/components/a1-accordion-faq') ?>
     <?php endwhile; ?>
 
 <?php endif; ?>
 
-
 <?php get_footer(); ?>
+
---
+++ inc/components/p2-picture-text.php
@@ -0,0 +1,23 @@
+<?php if( get_row_layout() == 'p2_picture_text' ): ?>
+
+        <div class="container-fluid site-component-container p2-container">
+            <?php while(have_rows('picture_text_rows')) : the_row();  
+                $image = get_sub_field('image');
+                $title = get_sub_field('title');
+                $content = get_sub_field('content');
+            ?>
+                <div class="row site-component-row p2-row">
+                    <div class="col-lg-4 image-column">
+                        <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                    </div>
+                    <div class="col-lg-6 content-column">
+                        <h2 class="p2-title title-text _33"><?php echo $title; ?></h2>
+                        <div class="p2-content body-text _21">
+                            <?php echo $content ?>
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        </div>
+
+<?php endif; ?>
